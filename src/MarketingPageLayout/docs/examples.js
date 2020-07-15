618274645235e4c3a82c7ef41e15313d30f37e94
alex-pust <oleksandrp@wix.com>
2020-07-15 16:44:37 +0300

    #DSM-65: Fix story examples

--- src/MarketingPageLayout/docs/examples.js
+++ src/MarketingPageLayout/docs/examples.js
@@ -1,27 +1,6 @@
-export const defaultViewport = `
-  <div style={{overflow: 'scroll'}}>
-    <div style={{width: '1500px', height: '630px'}}>
-      <MarketingPageLayout
-        overline="Overline"
-        header="Header Text"
-        subtitle="Subtitle Text"
-        content={[
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-        ]}
-        contentPrefixIcon={<Icons.Check />}
-        actions={<Button>Main Action</Button>}
-        footer={<div>Footer Content</div>}
-        image={<Image />}
-      />
-    </div>
-  </div>
-`;
-
 export const largeViewport = `
   <div style={{overflow: 'auto'}}>
-    <div style={{width: '1500px', height: '630px'}}>
+    <div style={{width: '1500px', height: '430px', overflow: 'auto'}}>
       <MarketingPageLayout
         overline="Overline"
         header="Header Text"
@@ -35,28 +14,28 @@ export const largeViewport = `
         size="large"
         actions={<Button>Main Action</Button>}
         footer={
-            <MarketingPageTestimonialsFooter
-                testimonials={[
-                  {
-                    id: '0001',
-                    avatar: <Avatar name="Guy in glasses" size="size60"/>,
-                    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-                    authorName: 'Guy in glasses'
-                  },
-                  {
-                    id: '0002',
-                    avatar: <Avatar name="Guy in glasses" size="size60"/>,
-                    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-                    authorName: 'Guy in glasses'
-                  },
-                  {
-                    id: '0003',
-                    avatar: <Avatar name="Guy in glasses" size="size60"/>,
-                    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-                    authorName: 'Guy in glasses'
-                  },
-                ]}
-            />
+          <MarketingPageTestimonialsFooter
+            testimonials={[
+              {
+                id: '0001',
+                avatar: <Avatar name="Guy in glasses" size="size60"/>,
+                text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+                authorName: 'Guy in glasses'
+              },
+              {
+                id: '0002',
+                avatar: <Avatar name="Guy in glasses" size="size60"/>,
+                text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+                authorName: 'Guy in glasses'
+              },
+              {
+                id: '0003',
+                avatar: <Avatar name="Guy in glasses" size="size60"/>,
+                text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+                authorName: 'Guy in glasses'
+              },
+            ]}
+          />
         }
         image={<Image />}
       />
@@ -65,91 +44,87 @@ export const largeViewport = `
 `;
 
 export const mediumViewport = `
-  <div style={{overflow: 'scroll'}}>
-    <div style={{width: '1100px', height: '430px'}}>
-      <MarketingPageLayout
-        overline="Overline"
-        header="Header Text"
-        subtitle="Subtitle Text"
-        content={[
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-        ]}
-        contentPrefixIcon={<Icons.Check />}
-        actions={<Button>Main Action</Button>}
-        size="medium"
-        footer={
-            <MarketingPageTestimonialsFooter
-                testimonials={[
-                  {
-                    id: '0001',
-                    avatar: <Avatar name="Guy in glasses" size="size60"/>,
-                    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-                    authorName: 'Guy in glasses'
-                  },
-                  {
-                    id: '0002',
-                    avatar: <Avatar name="Guy in glasses" size="size60"/>,
-                    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-                    authorName: 'Guy in glasses'
-                  },
-                  {
-                    id: '0003',
-                    avatar: <Avatar name="Guy in glasses" size="size60"/>,
-                    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-                    authorName: 'Guy in glasses'
-                  },
-                ]}
-            />
-        }
-        image={<Image />}
-      />
-    </div>
+  <div style={{width: '1100px', height: '430px', overflow: 'scroll'}}>
+    <MarketingPageLayout
+      overline="Overline"
+      header="Header Text"
+      subtitle="Subtitle Text"
+      content={[
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      ]}
+      contentPrefixIcon={<Icons.Check />}
+      actions={<Button>Main Action</Button>}
+      size="medium"
+      footer={
+        <MarketingPageTestimonialsFooter
+          testimonials={[
+            {
+              id: '0001',
+              avatar: <Avatar name="Guy in glasses" size="size60"/>,
+              text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+              authorName: 'Guy in glasses'
+            },
+            {
+              id: '0002',
+              avatar: <Avatar name="Guy in glasses" size="size60"/>,
+              text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+              authorName: 'Guy in glasses'
+            },
+            {
+              id: '0003',
+              avatar: <Avatar name="Guy in glasses" size="size60"/>,
+              text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+              authorName: 'Guy in glasses'
+            },
+          ]}
+        />
+      }
+      image={<Image />}
+    />
   </div>
 `;
 
 export const smallViewport = `
-  <div style={{overflow: 'auto'}}>
-    <div style={{width: '420px', height: '630px'}}>
-      <MarketingPageLayout
-        overline="Overline"
-        header="Header Text"
-        subtitle="Subtitle Text"
-        content={[
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-        ]}
-        contentPrefixIcon={<Icons.Check />}
-        size="small"
-        actions={<Button>Main Action</Button>}
-        footer={
-            <MarketingPageTestimonialsFooter
-                testimonials={[
-                  {
-                    id: '0001',
-                    avatar: <Avatar name="Guy in glasses" size="size60"/>,
-                    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-                    authorName: 'Guy in glasses'
-                  },
-                  {
-                    id: '0002',
-                    avatar: <Avatar name="Guy in glasses" size="size60"/>,
-                    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-                    authorName: 'Guy in glasses'
-                  },
-                  {
-                    id: '0003',
-                    avatar: <Avatar name="Guy in glasses" size="size60"/>,
-                    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-                    authorName: 'Guy in glasses'
-                  },
-                ]}
-            />
-        }
-        image={<Image />}
-      />
-    </div>
+  <div style={{width: '420px', height: '430px', overflow: 'auto'}}>
+    <MarketingPageLayout
+      overline="Overline"
+      header="Header Text"
+      subtitle="Subtitle Text"
+      content={[
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      ]}
+      contentPrefixIcon={<Icons.Check />}
+      size="small"
+      actions={<Button>Main Action</Button>}
+      footer={
+        <MarketingPageTestimonialsFooter
+          testimonials={[
+            {
+              id: '0001',
+              avatar: <Avatar name="Guy in glasses" size="size60"/>,
+              text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+              authorName: 'Guy in glasses'
+            },
+            {
+              id: '0002',
+              avatar: <Avatar name="Guy in glasses" size="size60"/>,
+              text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+              authorName: 'Guy in glasses'
+            },
+            {
+              id: '0003',
+              avatar: <Avatar name="Guy in glasses" size="size60"/>,
+              text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+              authorName: 'Guy in glasses'
+            },
+          ]}
+        />
+      }
+      image={<Image />}
+    />
   </div>
 `;
